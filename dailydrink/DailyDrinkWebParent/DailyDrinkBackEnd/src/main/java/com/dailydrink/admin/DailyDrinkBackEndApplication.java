d635f5e579ab0578948aca83fedebe5f91a95094
Darwin Tirta <darwin.hermawan@gmail.com>
2022-02-26 18:59:44 +0700

    update

---
+++ dailydrink/DailyDrinkWebParent/DailyDrinkBackEnd/src/main/java/com/dailydrink/admin/DailyDrinkBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dailydrink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDrinkBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyDrinkBackEndApplication.class, args);
+	}
+
+}
